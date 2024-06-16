d9ef7e97102baf80326847976a49dafaae9c718f
Nick Dowsett <nickdowsett42@gmail.com>
2024-06-17 07:26:21 +0800

    Skeleton for new GetPlaylist query

--- ytmapi-rs/src/query.rs
+++ ytmapi-rs/src/query.rs
@@ -189,3 +189,50 @@ pub mod watch {
         }
     }
 }
+
+pub mod playlist {
+    use super::Query;
+    use crate::{
+        common::{PlaylistID, YoutubeID},
+        VideoID,
+    };
+    use serde_json::json;
+    use std::borrow::Cow;
+
+    pub struct VideoAndPlaylistID<'a> {
+        video_id: VideoID<'a>,
+        playlist_id: PlaylistID<'a>,
+    }
+
+    pub struct GetPlaylistQuery<'a> {
+        id: PlaylistID<'a>,
+    }
+    impl<'a> Query for GetPlaylistQuery<'a> {
+        fn header(&self) -> serde_json::Map<String, serde_json::Value> {
+            todo!();
+            // let serde_json::Value::Object(map) = json!({
+            //     "enablePersistentPlaylistPanel": true,
+            //     "isAudioOnly": true,
+            //     "tunerSettingValue": "AUTOMIX_SETTING_NORMAL",
+            //     "videoId" : self.id.get_raw(),
+            //     "playlistId" : format!("RDAMVM{}",self.id.get_raw()),
+            // }) else {
+            //     unreachable!()
+            // };
+            // map
+        }
+        fn path(&self) -> &str {
+            todo!();
+            // "next"
+        }
+        fn params(&self) -> Option<Cow<str>> {
+            todo!();
+            // None
+        }
+    }
+    impl<'a> GetPlaylistQuery<'a> {
+        pub fn new(id: PlaylistID<'a>) -> GetPlaylistQuery<'a> {
+            GetPlaylistQuery { id }
+        }
+    }
+}
