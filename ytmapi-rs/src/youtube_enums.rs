33e81d9ffc4ca6c972b83be3dd0e9f053fa8fc29
Nick Dowsett <nickdowsett42@gmail.com>
2024-07-25 23:47:55 +0800

    Add missing file

---
+++ ytmapi-rs/src/youtube_enums.rs
@@ -0,0 +1,41 @@
+//! Module to contain YouTube enumerated values for internal use only.
+
+use serde::{Deserialize, Serialize};
+
+// watchPlaylistEndpoint params within overlay.
+// To distinguish between Community and Featured playlists for playlist search.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub(crate) enum PlaylistEndpointParams {
+    #[serde(rename = "wAEB")]
+    Featured,
+    #[serde(rename = "wAEB8gECKAE%3D")]
+    Community,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub(crate) enum YoutubeMusicVideoType {
+    // I believe OMV is 'Official Music Video' and UGC is 'User Generated Content'
+    #[serde(rename = "MUSIC_VIDEO_TYPE_UGC")]
+    Ugc,
+    #[serde(rename = "MUSIC_VIDEO_TYPE_OMV")]
+    Omv,
+    // Could be 'Audio Track Video'?
+    #[serde(rename = "MUSIC_VIDEO_TYPE_ATV")]
+    Atv,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub(crate) enum YoutubeMusicTableListVideoType {
+    // I believe OMV is 'Official Music Video' and UGC is 'User Generated Content'
+    #[serde(rename = "MUSIC_VIDEO_TYPE_UGC")]
+    Ugc,
+    #[serde(rename = "MUSIC_VIDEO_TYPE_OMV")]
+    Omv,
+    // Could be 'Audio Track Video'?
+    #[serde(rename = "MUSIC_VIDEO_TYPE_ATV")]
+    Atv,
+    #[serde(rename = "MUSIC_VIDEO_TYPE_PODCAST_EPISODE")]
+    Episode,
+    #[serde(rename = "MUSIC_VIDEO_TYPE_PRIVATELY_OWNED_TRACK")]
+    Upload,
+}
