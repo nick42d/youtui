4c07dadfe9bfc0b2f8a4e0a2e9a638c195c3a3ef
Nick Dowsett <nickdowsett42@gmail.com>
2024-09-04 23:34:16 +0800

    More fun with continuations

--- ytmapi-rs/src/query/continuations.rs
+++ ytmapi-rs/src/query/continuations.rs
@@ -1,37 +1,66 @@
 use super::{BasicSearch, GetQuery, PostMethod, PostQuery, Query, QueryMethod, SearchQuery};
 use crate::{
-    auth::AuthToken,
+    auth::{AuthToken, BrowserToken},
     parse::{ParseFrom, ProcessedResult},
     Result,
 };
 use async_stream::{stream, try_stream};
-use std::borrow::Cow;
+use std::{borrow::Cow, fmt::Debug};
 use tokio_stream::Stream;
 
 trait Continuable {
     fn get_continuation_params(&self) -> Option<String>;
 }
-trait StreamingQuery<A: AuthToken>: Query<A>
+// trait StreamingQuery<A: AuthToken>: Query<A>
+// where
+//     Self::Output: Continuable,
+// {
+//     fn stream(
+//         &self,
+//         client: &crate::client::Client,
+//         tok: &A,
+//     ) -> impl Stream<Item = Result<Self::Output>> {
+//         try_stream! {
+//             let first_res: Self::Output = Self::Method::call(self, client,
+// tok).await?.process()?.parse_into()?;             let first_cont_pars =
+// Continuable::get_continuation_params(&first_res);             yield
+// first_res;             if let Some(first_cont_pars) = first_cont_pars {
+//                 let query = GetContinuationsQuery {
+//                     continuation_params: first_cont_pars,
+//                     query: self
+//                 };
+//                 let next = <GetContinuationsQuery<'_, _> as
+// Query<A>>::Method::call(&query, client, tok).await?.process()?.parse_into()?;
+//             }
+//         }
+//     }
+// }
+
+fn stream<'a, Q: Query<BrowserToken>>(
+    query: Q,
+    client: &crate::client::Client,
+    tok: &BrowserToken,
+) -> impl Stream<Item = Result<Q::Output>>
 where
-    Self::Output: Continuable,
+    Q::Output: Continuable,
 {
-    fn stream(
-        &self,
-        client: &crate::client::Client,
-        tok: &A,
-    ) -> impl Stream<Item = Result<Self::Output>> {
-        try_stream! {
-            let first_res: Self::Output = Self::Method::call(self, client, tok).await?.process()?.parse_into()?;
-            let first_cont_pars = Continuable::get_continuation_params(&first_res);
-            yield first_res;
-            if let Some(first_cont_pars) = first_cont_pars {
-                let query = GetContinuationsQuery {
-                    continuation_params: first_cont_pars,
-                    query: self
-                };
-                let next = <GetContinuationsQuery<'_, _> as Query<A>>::Method::call(&query, client, tok).await?.process()?.parse_into()?;
-            }
+    try_stream! {
+        let first_res: Q::Output = Q::Method::call(&query, client, tok).await?.process()?.parse_into()?;
+        let first_cont_pars = first_res.get_continuation_params();
+        yield first_res;
+        if let Some(first_cont_pars) = first_cont_pars {
+            let query = GetContinuationsQuery {
+                continuation_params: first_cont_pars,
+                query: &query
+            };
+            let next = <GetContinuationsQuery<'_, _> as Query<BrowserToken>>::Method::call(&query, client, tok).await?.process()?.parse_into()?;
         }
+    }
+}
+
+impl<'a, Q, T: Debug> ParseFrom<GetContinuationsQuery<'a, Q>> for T {
+    fn parse_from(p: ProcessedResult<GetContinuationsQuery<'a, Q>>) -> crate::Result<Self> {
+        todo!()
     }
 }
 
@@ -41,9 +70,9 @@ pub struct GetContinuationsQuery<'a, Q> {
 }
 // TODO: Output type
 impl<'a, Q: Query<A>, A: AuthToken> Query<A> for GetContinuationsQuery<'a, Q>
-where
-    Q::Output: ParseFrom<Self>,
-    Q::Method: QueryMethod<Self, A, <Q as Query<A>>::Output>,
+// where
+//     Q::Output: ParseFrom<Self>,
+//     Q::Method: QueryMethod<Self, A, <Q as Query<A>>::Output>,
 {
     type Output = Q::Output;
     type Method = Q::Method;
