827f8b43bd526110e0d865627372a8a6c2ca2d9f
Nick Dowsett <nickdowsett42@gmail.com>
2024-10-12 19:55:20 +0800

    Add stream util for downloader

---
+++ youtui/src/app/server/downloader/utils.rs
@@ -0,0 +1,24 @@
+use bytes::Bytes;
+use rusty_ytdl::VideoError;
+
+pub fn into_futures_stream(
+    youtube_stream: Box<dyn rusty_ytdl::stream::Stream>,
+) -> impl futures::Stream<Item = Result<Bytes, VideoError>> {
+    // Second value of initialisation tuple represents if the previous iteration of
+    // the stream errored. If so, stream will close, as no future iterations of
+    // the stream are expected to return Ok.
+    futures::stream::unfold((youtube_stream, false), |(state, err)| async move {
+        if err {
+            return None;
+        };
+        let chunk = state.chunk().await;
+        match chunk {
+            // Return error value on this iteration, on the next iteration return None.
+            Err(e) => Some((Err(e), (state, true))),
+            // Happy path
+            Ok(Some(bytes)) => Some((Ok(bytes), (state, false))),
+            // Stream has closed.
+            Ok(None) => None,
+        }
+    })
+}
