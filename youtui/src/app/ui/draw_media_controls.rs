c55cd83de781d41671ded15f314b7206eec2f1fb
Nick Dowsett <nickdowsett42@gmail.com>
2025-05-30 21:59:38 +0800

    Rationalize draw_media_controls function

--- youtui/src/app/ui/draw_media_controls.rs
+++ youtui/src/app/ui/draw_media_controls.rs
@@ -6,10 +6,9 @@ use itertools::Itertools;
 use std::time::Duration;
 
 pub fn draw_app_media_controls(w: &YoutuiWindow) -> MediaControlsUpdate<'_> {
-    let cur = &w.playlist.play_status;
     let mut duration = 0;
     let mut progress = Duration::default();
-    match cur {
+    match &w.playlist.play_status {
         PlayState::Playing(id) | PlayState::Paused(id) => {
             duration = w
                 .playlist
@@ -22,61 +21,31 @@ pub fn draw_app_media_controls(w: &YoutuiWindow) -> MediaControlsUpdate<'_> {
         }
         _ => 0.0,
     };
-    let cur = &w.playlist.play_status;
-    let song_title = match w.playlist.play_status {
+    let cur_active_song = match w.playlist.play_status {
         PlayState::Error(id)
         | PlayState::Playing(id)
         | PlayState::Paused(id)
-        | PlayState::Buffering(id) => w
-            .playlist
-            .get_song_from_id(id)
-            .map(|s| s.title.as_ref())
-            .unwrap_or("No title"),
-        PlayState::NotPlaying => "Not playing",
-        PlayState::Stopped => "Not playing",
+        | PlayState::Buffering(id) => w.playlist.get_song_from_id(id),
+        PlayState::NotPlaying | PlayState::Stopped => None,
     };
-    let album_title = match w.playlist.play_status {
-        PlayState::Error(id)
-        | PlayState::Playing(id)
-        | PlayState::Paused(id)
-        | PlayState::Buffering(id) => w
-            .playlist
-            .get_song_from_id(id)
-            .and_then(|s| s.album.as_ref())
-            .map(|s| s.name.as_str())
-            .unwrap_or_default(),
-        PlayState::NotPlaying => "",
-        PlayState::Stopped => "",
-    };
-    let album_art_path = match w.playlist.play_status {
-        PlayState::Error(id)
-        | PlayState::Playing(id)
-        | PlayState::Paused(id)
-        | PlayState::Buffering(id) => w
-            .playlist
-            .get_song_from_id(id)
-            .and_then(|s| s.album_art.as_ref())
-            .map(|s| format!("file://{}", &s.on_disk_path.display())),
-        PlayState::NotPlaying => None,
-        PlayState::Stopped => None,
-    };
-    let artist_title = match w.playlist.play_status {
-        PlayState::Error(id)
-        | PlayState::Playing(id)
-        | PlayState::Paused(id)
-        | PlayState::Buffering(id) => w
-            .playlist
-            .get_song_from_id(id)
-            .map(|s| s.artists.as_ref())
-            .map(|s| {
-                Itertools::intersperse(s.iter().map(|s| s.name.as_str()), ", ").collect::<String>()
-            })
-            .unwrap_or("".to_string())
-            .into(),
-        PlayState::NotPlaying => "".into(),
-        PlayState::Stopped => "".into(),
-    };
-    let playback_status = match cur {
+    let song_title = cur_active_song
+        .map(|s| s.title.as_str())
+        .unwrap_or_default();
+    let album_title = cur_active_song
+        .and_then(|s| s.album.as_ref())
+        .map(|s| s.name.as_str())
+        .unwrap_or_default();
+    let album_art_path = cur_active_song
+        .and_then(|s| s.album_art.as_ref())
+        .map(|s| format!("file://{}", &s.on_disk_path.display()));
+    let artist_title = cur_active_song
+        .map(|s| s.artists.as_ref())
+        .map(|s| {
+            Itertools::intersperse(s.iter().map(|s| s.name.as_str()), ", ").collect::<String>()
+        })
+        .unwrap_or("".to_string())
+        .into();
+    let playback_status = match w.playlist.play_status {
         PlayState::Playing(_) => MediaControlsStatus::Playing { progress },
         PlayState::Paused(_) => MediaControlsStatus::Paused { progress },
         _ => MediaControlsStatus::Stopped,
