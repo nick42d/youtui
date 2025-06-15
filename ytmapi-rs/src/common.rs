7ecd5c7fdd06a556f867497ba255cb25767e5f7d
nick42d <133559267+nick42d@users.noreply.github.com>
2025-06-15 22:56:03 +0800

    chore: Fix small typos, and bump vernum for ytmapi-rs (#252)
    * chore: Fix small typos, and bump vernum for ytmapi-rs
    
    * Bump vernum

--- ytmapi-rs/src/common.rs
+++ ytmapi-rs/src/common.rs
@@ -175,8 +175,6 @@ pub struct UploadAlbumID<'a>(Cow<'a, str>);
 #[derive(PartialEq, Eq, Hash, Debug, Clone, Serialize, Deserialize)]
 pub struct UploadArtistID<'a>(Cow<'a, str>);
 #[derive(PartialEq, Eq, Hash, Debug, Clone, Serialize, Deserialize)]
-pub struct UploadUrl<'a>(Cow<'a, str>);
-#[derive(PartialEq, Eq, Hash, Debug, Clone, Serialize, Deserialize)]
 pub struct TasteTokenSelection<'a>(Cow<'a, str>);
 #[derive(PartialEq, Eq, Hash, Debug, Clone, Serialize, Deserialize)]
 pub struct TasteTokenImpression<'a>(Cow<'a, str>);
@@ -190,7 +188,6 @@ impl_youtube_id!(SetVideoID<'a>);
 impl_youtube_id!(AlbumID<'a>);
 impl_youtube_id!(UploadAlbumID<'a>);
 impl_youtube_id!(UploadArtistID<'a>);
-impl_youtube_id!(UploadUrl<'a>);
 impl_youtube_id!(ProfileID<'a>);
 impl_youtube_id!(PodcastID<'a>);
 impl_youtube_id!(EpisodeID<'a>);
