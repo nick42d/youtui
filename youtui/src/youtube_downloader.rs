212236a7168db6ea610d0354703305e5a128ab2c
Nick Dowsett <nickdowsett42@gmail.com>
2025-11-10 17:42:44 +0800

    Design new YoutubeDownloader trait and write test implementation for rusty_ytdl

--- youtui/src/youtube_downloader.rs
+++ youtui/src/youtube_downloader.rs
@@ -1,2 +1,66 @@
+use bytes::Bytes;
+use futures::{Stream, StreamExt};
+use rusty_ytdl::{Video, VideoError, VideoOptions};
+
+pub struct SongInformation {
+    total_size_bytes: usize,
+    chunk_size_bytes: u64,
+}
+
 pub trait YoutubeDownloader {
+    type Error;
+    async fn download_song<'a>(
+        &'a self,
+        song_video_id: impl Into<String>,
+    ) -> Result<(SongInformation, impl Stream<Item = Bytes> + 'a), Self::Error>;
+}
+
+pub struct NativeYoutubeDownloader {
+    options: VideoOptions,
+}
+
+impl YoutubeDownloader for NativeYoutubeDownloader {
+    type Error = ();
+    async fn download_song<'a>(
+        &'a self,
+        song_video_id: impl Into<String>,
+    ) -> Result<(SongInformation, impl Stream<Item = Bytes> + 'a), Self::Error> {
+        let Ok(video) = Video::new_with_options(song_video_id, &self.options) else {
+            todo!();
+        };
+        let stream = video.stream().await.unwrap();
+        let chunk_size_bytes = self.options.download_options.dl_chunk_size.unwrap();
+        let total_size_bytes = stream.content_length();
+        let stream = into_futures_stream(stream).map(|item| item.unwrap());
+        let song_information = SongInformation {
+            total_size_bytes,
+            chunk_size_bytes,
+        };
+        Ok((song_information, stream))
+    }
+}
+
+/// Helper function to use rusty_ytdl::stream::Stream is if it were a
+/// futures::Stream.
+// NOTE: Potentially could be upstreamed: https://github.com/Mithronn/rusty_ytdl/issues/34.
+pub fn into_futures_stream(
+    youtube_stream: Box<dyn rusty_ytdl::stream::Stream + Send>,
+) -> impl futures::Stream<Item = Result<Bytes, VideoError>> + Send {
+    // Second value of initialisation tuple represents if the previous iteration of
+    // the stream errored. If so, stream will close, as no future iterations of
+    // the stream are expected to return Ok.
+    futures::stream::unfold((youtube_stream, false), |(state, err)| async move {
+        if err {
+            return None;
+        };
+        let chunk = state.chunk().await;
+        match chunk {
+            // Return error value on this iteration, on the next iteration return None.
+            Err(e) => Some((Err(e), (state, true))),
+            // Happy path
+            Ok(Some(bytes)) => Some((Ok(bytes), (state, false))),
+            // Stream has closed.
+            Ok(None) => None,
+        }
+    })
 }
