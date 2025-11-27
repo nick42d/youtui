17467c89aced9dee22cf63abd72687c9dc1297a2
Nick Dowsett <nickdowsett42@gmail.com>
2025-11-27 18:40:15 +0800

    Complete custom TabGrid widget

--- youtui/src/app/ui/header.rs
+++ youtui/src/app/ui/header.rs
@@ -1,10 +1,12 @@
 use crate::app::component::actionhandler::{get_global_keybinds_as_readable_iter, KeyRouter};
-use crate::drawutils::{BUTTON_BG_COLOUR, BUTTON_FG_COLOUR};
+use crate::drawutils::{
+    BUTTON_BG_COLOUR, BUTTON_FG_COLOUR, DESELECTED_BORDER_COLOUR, SELECTED_BORDER_COLOUR,
+};
 use crate::keyaction::DisplayableKeyAction;
 use ratatui::layout::{Constraint, Layout, Rect};
 use ratatui::style::{Style, Stylize};
 use ratatui::text::{Line, Span};
-use ratatui::widgets::{Block, Borders, Paragraph};
+use ratatui::widgets::{Block, Borders, Paragraph, Wrap};
 use ratatui::Frame;
 use std::fmt::Debug;
 
@@ -29,11 +31,19 @@ pub fn draw_header(f: &mut Frame, w: &super::YoutuiWindow, chunk: Rect) {
         },
     ));
     let block = Block::default().borders(Borders::ALL).title("Commands");
-    let block2 = Block::default().borders(Borders::ALL).title("Mode");
-    let header = Paragraph::new(help_string);
+    let block2 = Block::default().borders(Borders::ALL).title("Mode (Tab↹)");
+    // .title_bottom("[S]+Tab↹");
+    let header = Paragraph::new(help_string).wrap(Wrap { trim: true });
     let split = Layout::horizontal([Constraint::Min(0), Constraint::Max(19)]).split(chunk);
-    let tabs =
-        crate::widgets::TabGrid::new_with_cols(["Search", "Library", "Playlist", "Other"], 2);
+    let selected = match w.context {
+        super::WindowContext::Browser => 0,
+        super::WindowContext::Playlist => 1,
+        super::WindowContext::Logs => 2,
+    };
+    let tabs = crate::widgets::TabGrid::new_with_cols(["Search", "Playlist", "Logs"], 2)
+        .select(selected)
+        // .with_selected_style(Style::new().fg(BUTTON_FG_COLOUR).bg(SELECTED_BORDER_COLOUR))
+        .with_selected_style(Style::new().fg(BUTTON_FG_COLOUR).bg(BUTTON_BG_COLOUR));
     f.render_widget(header, block.inner(split[0]));
     f.render_widget(tabs, block.inner(split[1]));
     f.render_widget(block, split[0]);
