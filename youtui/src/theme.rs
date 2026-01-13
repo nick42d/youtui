414f5a376ca16046ebae8579436571a05a1ad8a1
Nick Dowsett <nickdowsett42@gmail.com>
2026-01-13 20:34:46 +0800

    Initial impl

--- youtui/src/theme.rs
+++ youtui/src/theme.rs
@@ -1,3 +1,4 @@
+use ratatui::style::Color;
 use std::collections::HashMap;
 
 pub struct RgbColour {
@@ -6,20 +7,50 @@ pub struct RgbColour {
     b: u16,
 }
 
+pub struct Theme2<P> {
+    pallete: HashMap<P, RgbColour>,
+}
+
 pub struct Theme {
-    app_bg_colour: String,
-    selected_panel_border_colour: String,
-    selected_panel_text_colour: String,
-    deselected_panel_border_colour: String,
-    deselected_panel_text_colour: String,
-    selected_text_colour: String,
-    deselected_text_colour: String,
-    button_bg_colour: String,
-    button_fg_colour: String,
-    progress_bg_colour: String,
-    progress_fg_colour: String,
-    progress_text_colour: String,
-    table_headings_colour: String,
-    row_highlight_colour: String,
-    palette: HashMap<String, RgbColour>,
+    selected_panel_border_colour: Color,
+    selected_panel_text_colour: Color,
+    deselected_panel_border_colour: Color,
+    deselected_panel_text_colour: Color,
+    selected_text_colour: Color,
+    selected_text_bg_colour: Color,
+    deselected_text_colour: Color,
+    error_text_colour: Color,
+    warn_text_colour: Color,
+    info_text_colour: Color,
+    debug_text_colour: Color,
+    trace_text_colour: Color,
+    button_bg_colour: Color,
+    button_fg_colour: Color,
+    progress_bg_colour: Color,
+    progress_fg_colour: Color,
+    table_headings_colour: Color,
+}
+
+fn default_theme() -> Theme {
+    let selected_border_colour = Color::Cyan;
+    let deselected_border_colour = Color::Reset;
+    Theme {
+        selected_panel_border_colour: selected_border_colour,
+        selected_panel_text_colour: selected_border_colour,
+        deselected_panel_border_colour: deselected_border_colour,
+        deselected_panel_text_colour: deselected_border_colour,
+        selected_text_colour: Color::Cyan,
+        selected_text_bg_colour: Color::Green,
+        deselected_text_colour: Color::Reset,
+        error_text_colour: Color::Red,
+        warn_text_colour: Color::Yellow,
+        info_text_colour: Color::Cyan,
+        debug_text_colour: Color::LightCyan,
+        trace_text_colour: Color::Gray,
+        button_bg_colour: Color::Gray,
+        button_fg_colour: Color::Black,
+        progress_bg_colour: Color::DarkGray,
+        progress_fg_colour: Color::LightGreen,
+        table_headings_colour: Color::LightGreen,
+    }
 }
